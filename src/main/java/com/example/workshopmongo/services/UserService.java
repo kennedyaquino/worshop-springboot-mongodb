e29edb05c2346c6280bb96e6dc807f3961fdc5de
Kennedy Aquino <kennedy.eletronica.94@gmail.com>
2020-08-16 13:46:56 -0300

    Atualização de usuário com PUT

--- src/main/java/com/example/workshopmongo/services/UserService.java
+++ src/main/java/com/example/workshopmongo/services/UserService.java
@@ -13,28 +13,39 @@ import com.example.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
-		return repository.findAll();		
+
+	public List<User> findAll() {
+		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
-	public User insert(User obj) {
-		return repository.insert(obj);
+
+	public User insert(User user) {
+		return repository.insert(user);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repository.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+			User newUser = findById(obj.getId());
+			updateData(newUser, obj);
+			return repository.save(newUser);
+	}
+
+	private void updateData(User newUser, User obj) {
+		newUser.setName(obj.getName());
+		newUser.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
